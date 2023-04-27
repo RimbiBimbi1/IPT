b77c18f7a02ace96d01e028e4220ea887a063358
RimbiBimbi1 <103674299+RimbiBimbi1@users.noreply.github.com>
2023-04-27 09:37:44 +0200

    2023042701

--- src/main/java/pl/ipt/iptApp.java
+++ src/main/java/pl/ipt/iptApp.java
@@ -1,7 +1,9 @@
 package pl.ipt;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import pl.ipt.Converter.Converter;
 import pl.ipt.Painter.Painter;
@@ -31,13 +33,105 @@ public class iptApp {
             BufferedImage result = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
 
 
-
+            Mat matIn = Converter.BufferedImage2Mat(img);
+            Mat matOut = Converter.BufferedImage2Mat(result);
 
 
             Imgproc.cvtColor(
-                    Converter.BufferedImage2Mat(img),
-                    Converter.BufferedImage2Mat(result),
+                    matIn,
+                    matOut,
                     Imgproc.COLOR_RGB2GRAY );
+
+            matOut.copyTo(matIn);
+            matOut = new Mat();
+            Imgproc.GaussianBlur(
+                    matIn,
+                    matOut,
+                    new Size(5,5),
+                    1
+            );
+
+            matOut.copyTo(matIn);
+            matOut = new Mat();
+            Mat sobelX = new Mat();
+            Mat sobelY = new Mat();
+
+            Mat kernelX = new Mat(3,3, CvType.CV_32F){
+                {
+                    put(0,0,-1);
+                    put(0,1,0);
+                    put(0,2,1);
+
+                    put(1,0,-2);
+                    put(1,1,0);
+                    put(1,2,2);
+
+                    put(2,0,-1);
+                    put(2,1,0);
+                    put(2,2,1);
+                }
+            };
+
+            Mat kernelY = new Mat(3,3, CvType.CV_32F){
+                {
+                    put(0,0,-1);
+                    put(0,1,-2);
+                    put(0,2,-1);
+
+                    put(1,0,0);
+                    put(1,1,0);
+                    put(1,2,0);
+
+                    put(2,0,1);
+                    put(2,1,2);
+                    put(2,2,1);
+                }
+            };
+
+
+
+            Imgproc.filter2D(
+                    matIn,
+                    sobelX,
+                    -1,
+                    kernelX
+            );
+
+
+            Imgproc.filter2D(
+                    matIn,
+                    sobelY,
+                    -1,
+                    kernelY
+            );
+
+//            Imgproc.Sobel(
+//                    matIn,
+//                    sobelX,
+//                    -1,
+//                    1,
+//                    0,
+//                    3
+//            );
+//
+//
+//            Imgproc.Sobel(
+//                    matIn,
+//                    sobelY,
+//                    -1,
+//                    0,
+//                    1,
+//                    3
+//            );
+
+
+            Core.add(sobelX,sobelY,matOut);
+
+
+
+            result = Converter.Mat2BufferedImage(matOut);
+
+
 
             ImageIO.write(result,type,fileOutputStream);
 
